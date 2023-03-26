22fd20318ff9f3f43129187c237c56e1027c0172
jasonkayzk <271226192@qq.com>
2023-03-26 22:50:32 +0800

    feat: canal

--- autok3s.sh
+++
@@ -1,1 +0,0 @@
-docker run -itd --restart=always --name=my-autok3s --net host -v /var/run/docker.sock:/var/run/docker.sock cnrancher/autok3s:v0.5.2
--- syncthing.sh
+++
@@ -1,1 +0,0 @@
-docker run -itd --network=host -v /root/data/docker-volumn/sync-thing:/var/syncthing --restart=always --name=my-syncthing --privileged=true syncthing/syncthing:1.22
