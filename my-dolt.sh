f400b3fc71d3a51909f6dfac5fbeda2212afc7c9
Jasonkay <271226192@qq.com>
2024-01-21 17:11:14 +0800

    Update my-dolt.sh

--- my-dolt.sh
+++ my-dolt.sh
@@ -1,6 +1,29 @@
 export DOLT_HOME=/root/workspace/dolt
 
 mkdir -p $DOLT_HOME
+mkdir -p $DOLT_HOME/server-conf
+
+cat > $DOLT_HOME/server-conf/config.yaml << EOF
+log_level: info
+
+behavior:
+  read_only: false
+  autocommit: true
+
+user:
+  name: root
+  password: "your-password"
+
+listener:
+  host: localhost
+  port: 3306
+  max_connections: 100
+  read_timeout_millis: 28800000
+  write_timeout_millis: 28800000
+
+performance:
+  query_parallelism: null
+EOF
 
 docker run -itd --restart=always \
   --name my-dolt \
@@ -10,4 +33,4 @@ docker run -itd --restart=always \
   -v $DOLT_HOME/databases:/var/lib/dolt \
   dolthub/dolt-sql-server:1.32.0
 
-# Change config, and restart container...
+# Change config need restart the container...
