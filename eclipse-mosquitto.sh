89c9950ccc5654970f7dcd904af30d9cf99860ea
Jasonkay <271226192@qq.com>
2024-03-07 21:47:20 +0800

    Update eclipse-mosquitto.sh

--- eclipse-mosquitto.sh
+++ eclipse-mosquitto.sh
@@ -1,1 +1,17 @@
 docker run -itd --restart=always -p 1883:1883 -p 9001:9001 --name my-eclipse-mosquitto eclipse-mosquitto
+
+###### In Container ######
+## Create user & passwd
+## https://mosquitto.org/documentation/authentication-methods/
+docker exec -it my-eclipse-mosquitto /bin/sh
+cd /mosquitto
+mosquitto_passwd -c password_file <username>
+## Input passwd ...
+
+## Change config
+vi /mosquitto/config/mosquitto.conf
+### listener 1883
+### password_file /mosquitto/password_file
+
+# Restart containner
+docker restart my-eclipse-mosquitto
