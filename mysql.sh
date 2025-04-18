443f334211efbfb7c378f50a8f2464e44a3d0ef3
Jasonkay <271226192@qq.com>
2025-04-18 15:53:48 +0800

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,1 +1,1 @@
-docker run -itd --restart=always --name my-mysql -p 13306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql:5.7.39
+docker run -itd --restart=always --name my-mysql -p 13306:3306 -e MYSQL_ROOT_PASSWORD=123456 registry.cn-hangzhou.aliyuncs.com/jasonkay/mysql:8.0.39
